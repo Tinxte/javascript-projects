1dd8c04fd12b3c3df3c28ccee2d6965b03a33ed4
unknown <150742495+Tinxte@users.noreply.github.com>
2024-01-29 20:54:42 -0600

    More on functions studio

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -5,6 +5,15 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
+function findMinValue(arr) {
+let firstNumber = arr[0]
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < firstNumber) {
+        firstNumber = arr[i];
+}
+} return firstNumber;
+}
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(findMinValue(nums3));
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -20,10 +20,31 @@ function findMinValue(arr){
 
 //Your function here...
 
+let nums1 = [5, 10, 2, 42];
+let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+
+
+function sortArray(arr) {
+
+  let newArray = [];
+  
+  while (arr.length > 0) {
+  newArray.push(findMinValue(arr));
+  arr.splice[i];
+  }
+}
+
+
+console.log(sortArray(nums1));
+
+
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
 //Sample arrays for testing:
-let nums1 = [5, 10, 2, 42];
-let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+// let nums1 = [5, 10, 2, 42];
+// let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+// let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
