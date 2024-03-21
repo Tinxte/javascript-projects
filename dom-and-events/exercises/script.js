b315e5e4a9fe267fd6621feaf8b6bd1410de1dc6
unknown <150742495+Tinxte@users.noreply.github.com>
2024-03-21 12:15:12 -0500

    DOM and Events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,27 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    // Takeoff button
+
+    button.addEventListener("click", event =>
+    paragraph.innerHTML = `Houston, we have liftoff!`
+    );
+
+    // Mission Abort button functions
+
+    missionAbort.addEventListener("mouseover", event =>
+    missionAbort.style.color = "red"
+    );
+    missionAbort.addEventListener("mouseout", event =>
+    missionAbort.style.color = ""
+    )
+
+    missionAbort.addEventListener("click", function (event) {
+    let response = confirm("Are you sure you want to abort the mission?")
+    if (response) {
+        paragraph.innerHTML = `Mission aborted! Space shuttle returning home`
+    }}
+    )
 }
 
 window.addEventListener("load", init);
